aa4b667cfe0ffa1a1f99ff4a37ff3234c772d46a
desffh <yangju058@gmail.com>
2025-03-05 15:41:30 +0900

    (2025.3.5) 애니메이션 이벤트 시스템

--- Program/Assets/Class10th (SoundSystem)/Scripts/AudioManager.cs
+++ Program/Assets/Class10th (SoundSystem)/Scripts/AudioManager.cs
@@ -6,9 +6,26 @@ public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
 
+
+    private static AudioManager instance;
+
+    // AudioManger 타입의 정적 참조 변수
+    public static AudioManager Instance { get { return instance; } }
+
     private void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        // 맨 처음 참조변수는 null을 가리킴 (데이터 안넣은 상태)
+        // -> null이라면 참조변수가 자기 자신을 가리키게
+        if(instance == null)
+        {
+            instance = this;
+            Debug.Log(Instance);
+        }
+        else // null이 아니라면
+        {
+
+        }
     }
 
     public void Listener(AudioClip audioClip)
--- Program/Assets/Class10th (SoundSystem)/Scripts/Sound.cs
+++ Program/Assets/Class10th (SoundSystem)/Scripts/Sound.cs
@@ -7,18 +7,26 @@ public class Sound : MonoBehaviour
     [SerializeField] int random;
     [SerializeField] AudioClip audioClip;
 
+    [SerializeField] WaitForSeconds waitForSeconds;
+
+
+
     private void Start()
     {
-        StartCoroutine(Coroutine()); 
+        StartCoroutine(Coroutine());
+
+        waitForSeconds = new WaitForSeconds(5.0f);
     }
     IEnumerator Coroutine()
     {
+        // while문 들어가기전에 미리 참조변수 동적 할당 
+
         while(true)
         {
-            yield return new WaitForSeconds(5f);
+            yield return waitForSeconds;
 
-            random = Random.Range(1, 6);
-            Debug.Log(random);
+            AudioManager.Instance.Listener(audioClip);
+            
         }
     }
 }
